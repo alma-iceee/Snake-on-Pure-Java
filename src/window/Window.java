ea3b36a8f7a65e753eac47e2dd197279015aefec
alma-iceee <alma.iceee@gmail.com>
2021-02-10 15:28:59 +0600

    Creating Window (JFrame) class

---
+++ src/window/Window.java
@@ -0,0 +1,19 @@
+package window;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Window {
+    public Window(Dimension size, String title, Game game) {
+        JFrame frame = new JFrame(title);
+        frame.setPreferredSize(size);
+        frame.setMaximumSize(size);
+        frame.setMinimumSize(size);
+
+        frame.add(game);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
