bbe7a525301a494db401a4b460d2a5e4b27c4218
alma-iceee <alma.iceee@gmail.com>
2021-02-12 18:38:38 +0600

    Fixes in Game.java

--- src/window/Game.java
+++ src/window/Game.java
@@ -1,15 +1,23 @@
 package window;
 
 import java.awt.*;
+import java.awt.image.BufferStrategy;
 
 public class Game extends Canvas implements Runnable {
-    public boolean isRunning = false;
-    public Thread thread;
+    private boolean isRunning = false;
+    private Thread thread;
 
     public Game() {
         new Window(new Dimension(600,600), "snake", this);
+
+        init();
+
+        start();
     }
 
+    private void init() {
+
+    }
 
     private synchronized void start() {
         isRunning = true;
@@ -31,7 +39,7 @@ public class Game extends Canvas implements Runnable {
     public void run() {
         this.requestFocus();
         long lastTime = System.nanoTime();
-        double amountOfTicks = 60.0;
+        double amountOfTicks = 10.0;
         double ns = 1000000000 / amountOfTicks;
         double delta = 0;
         long timer = System.currentTimeMillis();
@@ -55,9 +63,23 @@ public class Game extends Canvas implements Runnable {
     }
 
     private void tick() {
+
     }
 
     private void render() {
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, 600, 600);
+
+        g.dispose();
+        bs.show();
     }
 
     public static void main(String[] args) {
